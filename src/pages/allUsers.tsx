fd0911812ce7f1bad50327f81fef550c4f9a68c7
premnath <prem.netaji.super@gmail.com>
2024-10-10 14:46:00 +0530

    changes for remote 1

--- src/pages/allUsers.tsx
+++ src/pages/allUsers.tsx
@@ -109,7 +109,7 @@ function UserDetailTable({
         }
     }
 
-    if (userList.length === 0) {
+    if (userList?.length === 0) {
         return <EmptyComponent />;
     }
 
@@ -137,7 +137,7 @@ const AllUsers: React.FC = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getCodesDtl());
-    }, [dispatch, codesDtl.length == 0]);
+    }, [dispatch, codesDtl?.length == 0]);
     const columns: ColumnConfig[] = [
         {
             key: 'bh_user_first_name',

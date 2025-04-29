df42caec1c9ccbef232b88af9748ba3c44315962
bh-dhanushpathi-git <dhanushpathi@bighammer.ai>
2025-04-29 10:58:59 +0530

    ui fix

--- src/features/dataops/dashboard/SortableChartCard.tsx
+++ src/features/dataops/dashboard/SortableChartCard.tsx
@@ -36,7 +36,7 @@ export const SortableChartCard: React.FC<SortableChartCardProps> = ({
   title, 
   children, 
   className,
-  defaultHeight = 130,
+  defaultHeight = 300,
   onSaveHeight
 }) => {
   const [height, setHeight] = useState(defaultHeight);

37abdbf1fdb2bafd29593b8fb9d0bf09e8330696
bh-dhanushpathi-git <dhanushpathi@bighammer.ai>
2025-05-14 15:05:25 +0530

    update

--- src/features/data-catalog/components/ImportDataSourceWizard.tsx
+++ src/features/data-catalog/components/ImportDataSourceWizard.tsx
@@ -171,7 +171,7 @@ console.log(props.gitProjectList)
     const createDataSourceLayout = async (data: any) => {
         try {
             return await apiService.post({
-                url: '/data_source_layout',
+                url: '/data_source_layout/',
                 data: data,
                 usePrefix: true,
                 portNumber: CATALOG_API_PORT,

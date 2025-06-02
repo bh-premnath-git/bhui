71a6ee6ddab69c1f6fc31b9f43c20c597e95d4ba
Vinay Agarwal <vinay7agarwal@gmail.com>
2025-06-02 10:58:20 +0530

    Updated entrypoint shell script for EKS

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ if [ -z "$CODEARTIFACT_AUTH_TOKEN" ]; then
     --query authorizationToken \
     --output text
   )
-  echo "Its inside"
+  eccho "Its inside"
 else
   echo "Using existing CODEARTIFACT_AUTH_TOKEN"
 fi

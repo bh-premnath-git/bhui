a0ad21ab1598094c6e73ff692f5faf0869b3d8b3
bh-premnath-git <Premnath@bighammer.ai>
2025-06-11 06:18:07 +0530

    fixes

--- src/components/shared/GenericChatUI.tsx
+++ src/components/shared/GenericChatUI.tsx
@@ -10,7 +10,6 @@ import { useRecommendation } from '@/hooks/useRecommendation';
 import { LoadingState } from '@/components/shared/LoadingState';
 import { marked } from 'marked';
 import { createShortUUID } from '@/lib/utils';
-import { useDataOps } from '@/context/dataops/DataOpsContext';
   
 
 interface GenericChatUIProps {

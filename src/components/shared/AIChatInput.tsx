a4a346ece4034f22881b7642b9f18c9379afcca0
bh-dhanushpathi-git <dhanushpathi@bighammer.ai>
2025-06-12 17:04:42 +0000

    update

--- src/components/shared/AIChatInput.tsx
+++ src/components/shared/AIChatInput.tsx
@@ -70,7 +70,6 @@ export function AIChatInput({
     el.style.height = "auto";
     el.style.height = `${Math.min(el.scrollHeight, 250)}px`;
   };
-  debugger;
 
   useEffect(() => {
     if (textareaRef.current) {

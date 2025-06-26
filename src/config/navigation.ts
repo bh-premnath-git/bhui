1d2ed65e434f32e5e70b9efc2a98f0b6a1fb9137
bh-premnath-git <Premnath@bighammer.ai>
2025-06-26 16:38:04 +0530

    user admin page under construction

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -98,12 +98,12 @@ export const navigationItems: NavItem[] = [
     icon: Settings,
     path: ROUTES.ADMIN.INDEX,
     subItems: [
-      /* {
+      {
         title: "Manage Users",
         icon: Users,
         path: ROUTES.ADMIN.USERS.INDEX,
         parent: ROUTES.ADMIN.INDEX,
-      }, */
+      },
       {
         title: "Manage Projects",
         icon: FolderGit2,

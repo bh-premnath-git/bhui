5c80cc7199794e7e09edbf6c11b1b29290dc34e5
unknown <kiran@bighammer.ai>
2025-07-11 13:32:07 +0530

    BHAI-303 In create a module for assign-to and resolution reason in alter-hub

--- src/features/dataops/alertsHubs/hooks/usealertHub.ts
+++ src/features/dataops/alertsHubs/hooks/usealertHub.ts
@@ -1,23 +1,73 @@
+// import { useResource } from "@/hooks/api/useResource";
+// import { AlertHub } from "@/types/dataops/alertsHub";
+// import { MONITOR_REMOTE_URL } from "@/config/platformenv";
+
+// interface UseAlertHubOptions {
+//     shouldFetch?: boolean;
+// }
+
+// export const useAlertHub = (options: UseAlertHubOptions = { shouldFetch: true }) => {
+//     const {
+//         getAll,
+//     } = useResource<AlertHub>('alert', MONITOR_REMOTE_URL, true);
+
+//     const { data: alertHub, isLoading, isFetching, isError } = getAll({ url: '/alert/' });
+
+
+//     return {
+//         alertHub,
+//         isLoading,
+//         isFetching,
+//         isError,
+//     };
+// }
+
+import { useCallback } from 'react';
 import { useResource } from "@/hooks/api/useResource";
 import { AlertHub } from "@/types/dataops/alertsHub";
 import { MONITOR_REMOTE_URL } from "@/config/platformenv";
+import { toast } from 'sonner';
 
 interface UseAlertHubOptions {
-    shouldFetch?: boolean;
+  shouldFetch?: boolean;
 }
 
 export const useAlertHub = (options: UseAlertHubOptions = { shouldFetch: true }) => {
-    const {
-        getAll,
-    } = useResource<AlertHub>('alert', MONITOR_REMOTE_URL, true);
+  const {
+    getAll,
+    update,
+  } = useResource<AlertHub>('alert', MONITOR_REMOTE_URL, true);
 
-    const { data: alertHub, isLoading, isFetching, isError } = getAll({ url: '/alert/' });
+  // Fetch all alerts
+  const { data: alertHub, isLoading, isFetching, isError, error} = getAll({
+    url: '/alert/',
+    queryOptions: {
+      enabled: options.shouldFetch,
+      retry: 2,
+    },
+  });
 
+  // console.log(alertHub);
+  // Update alert mutation
+  const updateAlertMutation = update('/alert', {
+    mutationOptions: {
+      onSuccess: () => toast.success('Alert updated successfully'),
+      onError: (err) => {
+        console.error('Update failed', err);
+        toast.error('Failed to update alert');
+      },
+    },
+  });
+  const handleUpdateAlert = useCallback(async (id: string, data: AlertHub) => {
+    await updateAlertMutation.mutateAsync({ data, params: { id } });
+  }, [updateAlertMutation]);
 
-    return {
-        alertHub,
-        isLoading,
-        isFetching,
-        isError,
-    };
-}
+  return {
+    alertHub: alertHub ?? [],
+    isLoading,
+    isFetching,
+    isError,
+    error,
+    handleUpdateAlert,
+  };
+};
